ce2eb789768ebb1d2c529a29a14571b50f316b1a
tobleric <tobler.eric@gmail.com>
2022-01-31 15:23:00 +0100

    commit space

--- sketch.js
+++ sketch.js
@@ -1,64 +1,99 @@
-function setup() {
-    angleMode(DEGREES)
-    createCanvas(400, 400, WEBGL);
-    frameRate(20)
+class Sphere {
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
 
   }
 
-  /*Startwinkel für Sekundenkreis*/
-  let a = 0;
+  show() {
+    push()
+    translate(this.x, this.y, this.z)
+    texture(texStars)
+    sphere(20)
+    pop()
+  }
 
-   /*Startwinkel für StundenkreisA/B*/
-  let b = 0;
-  let blue = 255;
+}
+
+/*Startwinkel für Sekundenkreis*/
+let a = 0;
+
+/*Startwinkel für StundenkreisA/B*/
+let b = 0;
+
+/*Array Sterne generieren */
+let stars = [];
+let texStars;
+
+function setup() {
+    angleMode(DEGREES)
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    frameRate(20)
+
+    texStars = loadImage("assets/texStars.png")
+
+    for (let i = 0; i < 20; i++) {
+      let starX = Math.floor(Math.random() * window.innerWidth)
+      let starY = Math.floor((Math.random() * window.innerHeight) - window.innerHeight/2)
+      let starZ = Math.floor((Math.random() * window.innerWidth) - 200)
+      stars.push(new Sphere(starX, starY, starZ))
+    }
+
+  }
   
   function draw() {
-    background(20)
-    orbitControl()
-    translate(-200, 0);
+    background(0)
+    // orbitControl()
+    translate(-windowWidth/2, 0);
     noStroke()
 
     /*Erstelle Radius für Stundekreis */
     let diameter = width;
     let radius = diameter/2;
+    let hourSize = 2
+    let sekSize = 20
 
     /*Generiere Licht*/
-    directionalLight(41, 0, 0, 0.5, 200, -200, -100)
-    directionalLight(0, 49, 0, 0.5, 200, -200, -100)
-    directionalLight(0, 0, 51, 0.5, 200, -200, -100)
+    pointLight(255, 255, 255, 200, -200, 100)
+    pointLight(25, 0, 0, -200, 200, -100)
+    ambientMaterial(255, 255, 255)
+    fill(255)
 
     /*Stelle Kamera*/
-    camera(0, 0, 250, width/2-100, 0, 0, 0, 1, 0)
+    // camera(0, 0, -300, 0, 0, 0, 0, 1, 0)
 
     //Erstelle Werte und Form Stundenkreis A
     push()
-    specularMaterial(41, 49, 51)
     let hourFact = cos(b)
     let hourAPosX = map(hourFact, 1, -1, width/4, width/2)
     translate(hourAPosX, 0, 0)
-    torus(radius/2, 12, 50)
+    torus(radius/2, hourSize, 70)
     pop()
 
     //Erstelle Werte und Form Stundenkreis B
     push()
-    specularMaterial(41, 49, 51)
     let hourBPosX = map(hourFact, -1, 1, width/2, width-100)
     translate(hourBPosX, 0, -100)
-    torus(radius/2, 12, 50)
+    torus(radius/2, hourSize, 50)
     pop()
 
     //Erstelle Werte und Form Sekundenkreis
     push()
-    directionalLight(0, 0, 50, 0.1, 200, -200, -100)
-    directionalLight(20, 0, 0, 0.2, -200, 200, -100)
-    specularMaterial(20, 0, 255)
     translate(hourAPosX, 0, 0)
     let sekPosY = sin(a) * radius / 2
     let sekPosX = cos(a) * radius / 2
 
     push()
     translate(sekPosX, sekPosY, 0)
-    sphere(30)
+    rotateX(90)
+
+    rotateY(frameCount/1.6666666667)
+
+    ambientMaterial(255)
+    torus(40)
+    /*Torus Light */
+  
     pop()
     pop()
 
@@ -68,5 +103,11 @@ function setup() {
     /*Speed von Stundenkreis wird erstellt*/
     b += 360/1200
 
-    //console.log(hourFact)
+    /*Stars generieren*/
+    for (let i = 0; i < stars.length; i++) {
+      stars[i].show()
+    }
+  
+
   }
+
